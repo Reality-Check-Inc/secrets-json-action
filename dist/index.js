6ba7a5a3c55ca2feab7dd5d0b9a1ea9d3d40235e
David N. Junod <djunod2@yahoo.com>
2024-12-18 15:52:09 -0700

    Version

--- dist/index.js
+++ dist/index.js
@@ -38652,8 +38652,8 @@ try {
           if (err) throw err;
           console.log(`${manifest} xml ${result}`);
           console.log(`Package.Identity ${result.Package.Identity}`);
-          console.log(`Package.Identity.Version ${result.Package.Identity['@Version']}`);
-          result.Package.Identity['@Version'] = buildVersion;
+          console.log(`Package.Identity.Version ${result.Package.Identity['Version']}`);
+          result.Package.Identity['Version'] = buildVersion;
           const contents = builder.buildObject(result);
           fs.writeFile(manifest, contents, err => {
             if (err) {
